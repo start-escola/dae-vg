27e19ba1e634d68bfc0d98b2513a54e0d49983da
wilsonmesquita03 <wilsonbrasilplauforever@gmail.com>
2024-07-18 15:27:44 -0300

    fix script

--- nginx/start-nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-envsubst '$DOMAIN $SSL_CERTIFICATE_PATH $SSL_CERTIFICATE_KEY_PATH' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
-mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
-
-nginx -g 'daemon off;'

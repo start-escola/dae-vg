cf7f28634db553501a5bc9ab0c96b296c1bbb7af
wilsonmesquita03 <wilsonbrasilplauforever@gmail.com>
2024-07-23 11:04:32 -0300

    fix init letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,79 +1,80 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+if ! [ -x "$(command -v docker compose)" ]; then
+  echo 'Error: docker compose is not installed.' >&2
   exit 1
 fi
 
-domains=(dae.varzeagrande.mt.gov.br www.dae.varzeagrande.mt.gov.br)
+domains=(dae.varzeagrande.mt.gov www.dae.varzeagrande.mt.gov)
 rsa_key_size=4096
 data_path="./certbot"
-email="wilson.mesquita@s2corporate.com.br" # Adicionar um endereço válido é fortemente recomendado
-staging=0 # Defina como 1 se estiver testando sua configuração para evitar atingir limites de solicitação
+email="wilson.mesquita@s2corporate.com.br" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Dados existentes encontrados para $domains. Continuar e substituir o certificado existente? (y/N) " decision
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
 fi
 
+
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Baixando parâmetros TLS recomendados ..."
+  echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
-echo "### Criando certificado dummy para $domains ..."
+echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
+docker compose run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 echo
 
-echo "### Iniciando nginx ..."
-docker-compose up --force-recreate -d nginx
+
+echo "### Starting nginx ..."
+docker compose up --force-recreate -d nginx
 echo
 
-echo "### Deletando certificado dummy para $domains ..."
-docker-compose run --rm --entrypoint "\
+echo "### Deleting dummy certificate for $domains ..."
+docker compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
 echo
 
-echo "### Solicitando certificado Let's Encrypt para $domains ..."
-# Junta $domains a argumentos -d
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
 domain_args=""
 for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
 done
 
-# Seleciona o argumento de email apropriado
+# Select appropriate email arg
 case "$email" in
   "") email_arg="--register-unsafely-without-email" ;;
   *) email_arg="--email $email" ;;
 esac
 
-# Habilita o modo de teste se necessário
+# Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
-    --force-renewal \
-    --preferred-challenges http \
-    --ipv4" certbot
+    --force-renewal" certbot
 echo
 
-echo "### Recarregando nginx ..."
-docker-compose exec nginx nginx -s reload
+echo "### Reloading nginx ..."
+docker compose exec nginx nginx -s reload
